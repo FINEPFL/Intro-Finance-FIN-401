3bf3a75aff43386931c0936f7fab677bb251941c
Mengjie <mengjie.zhao@outlook.com>
2016-09-27 19:52:30 +0200

    formalisation and move to new methods. needing better visualisation methods

--- lectureNotes/optProdPoint.py
+++ lectureNotes/optProdPoint.py
@@ -1,29 +1,59 @@
+from __future__ import print_function
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 class pushBgtConstr():
-    def __init__(self, bankRate=0.2, iniVal=1):
-        self.rate = bankRate
-        self.oriCapital = iniVal
-        self.oriYouthVal = np.linspace(0.0, 1.0, 10)
+    ''' These codes are intent to show how we can maximise net present value (NPV).
+    The implicit assumptions is that there are only two time phases and there is a
+    bank that allows people to map the value of money between now and then.
+    The principle is that when the rate of return of project is smaller than rate
+    of bank, stop investing. In addition, it is assumed that the investments are
+    not decided by preferences.
+    '''
+    def __init__(self, bankRate=0.2, iniVal=1, prjRateList=[.5, .4, .3, .2, .1]
+                                    , prjCostList=[.01, .05, .1, .15, .2]):
+        ''' Initialing parameters.
+        prjRateList contains rate of return of different proposed investing prjs.
+        prjCostList contains corresponding cost of prjs.
+        '''
+        self.bankRate = bankRate
+        self.prjRateList = prjRateList
+        self.prjCostList = prjCostList
+        self.iniVal = iniVal
 
-    def getOAValue(self, youthVal, capital):
-        oldAgeValue = (1+self.rate)*capital - (1+self.rate)*youthVal
-        return oldAgeValue
+    def getOAVbank(self, sumCurrent):
+        ''' It is the bank that allows you to manipulate the money '''
+        OAVbank = (1 + self.bankRate) * sumCurrent
+        return OAVbank
 
-    def prjInvest(self, prjProfit):
-        profitPV = prjProfit/(1 + self.rate)
-        newCapital = self.oriCapital + profitPV
-        newYouthVal = np.append(self.oriYouthVal, newCapital)
-        self.demon(newYouthVal, newCapital)
+    def getOAVprj(self):
+        ''' we do prjs whose rate of return is larger than that of the bank'''
+        info = np.asarray([self.prjRateList, self.prjCostList]).T
+        info = np.asarray(sorted(info, key=lambda rateList: rateList[0],
+                                                    reverse=True))
+        returnPrjOA = 0
+        returnCombined = 0
+        currentValYA = self.iniVal
+        investments = 0
 
-    def demon(self, newYouthVal, newCapital):
-        plt.plot(self.oriYouthVal, self.getOAValue(self.oriYouthVal, self.oriCapital))
-        plt.plot(newYouthVal, self.getOAValue(newYouthVal, newCapital))
-        plt.grid('on')
-        plt.show()
+        for i in xrange(0, info.shape[0]):
+            if info[i][0] >= self.bankRate:
+                returnPrjOA = returnPrjOA + (info[i][0]+1)*info[i][1]
+                currentValYA = currentValYA - info[i][1]
+                returnCombined = returnPrjOA
+                investments = investments + info[i][1]
+            else:
+                returnCombined = returnCombined + (self.bankRate+1) * currentValYA
+                break
 
+        self.returnCombined = returnCombined
+        self.returnPrjOA = returnPrjOA
+        self.amountOfInvestment = investments
 
 if __name__ == '__main__':
     ele1 = pushBgtConstr()
-    ele1.prjInvest(0.4)
+    ele1.getOAVprj()
+    print ("The combined (maximised) NPV is: ", ele1.returnCombined)
+    print ("The amount of return from project is: ", ele1.returnPrjOA)
+    print ("The amount of investments is: ", ele1.amountOfInvestment)
