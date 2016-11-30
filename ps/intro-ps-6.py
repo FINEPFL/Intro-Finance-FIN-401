f08d794412a52c81f7644823ada530c469935421
Mengjie <mengjie.zhao@outlook.com>
2016-11-30 23:24:01 +0100

    clean up and add suisse

--- ps/intro-ps-6.py
+++ ps/intro-ps-6.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from scipy.stats import linregress as lr
+from matplotlib import pyplot as plt
 
 import numpy as np
 import os
@@ -7,28 +8,39 @@ import io
 
 def parser(filename):
     with open(filename, 'rb') as f:
-        rows = f.readlines()[1:10]
+        rows = f.readlines()
         rows.pop(0)
         pre_price = 1
-        Date = []
+        pre_rm = 1
         r_risk = []
-        SP500 = []
+        rm_rate = []
         for row in rows:
             try:
-                date, _, _, _, stock_price, _, _, sp500 = row.strip().split(",")
-                Date.append(date)
-                SP500.append(float(sp500))
+                date, _, _, _, stock_price, _, _, r_m = row.strip().split(",")
+
+                rm_rate.append(float(r_m)/pre_rm - 1)
                 r_risk.append(float(stock_price)/pre_price - 1)
+
                 pre_price = float(stock_price)
-                # print r_risk, SP500
+                pre_rm = float(r_m)
+
             except BaseException:
                 print 'skipping one line!'
+
         r_risk.pop(0)
-        SP500.pop(0)
+        rm_rate.pop(0)
+        return r_risk, rm_rate
 
-        return r_risk, SP500
+ibm_r, SP500_rate = parser('ibm.csv')
+us_risk_free_rate = 0.77/365
+SP500_rate[:] = [x * 100 - us_risk_free_rate for x in SP500_rate]
+slope_us, intercept_us, _, _, _ = lr(SP500_rate, ibm_r)
+print slope_us, intercept_us
+print np.var(ibm_r)/np.cov(SP500_rate, ibm_r)[1,1]
 
-ibm_r, SP500 = parser('ibm.csv')
-
-slope, intercept, r_value, p_value, std_err = lr(SP500, ibm_r)
-print slope, r_value
+swisscom_r, SMI_rate = parser('swisscom.csv')
+swiss_risk_free_rate = -0.8
+SMI_rate[:] = [x * 100 - swiss_risk_free_rate for x in SMI_rate]
+slope_swi, intercept_swi, _, _, _ = lr(SMI_rate, swisscom_r)
+print slope_swi, intercept_swi
+print np.var(swisscom_r)/np.cov(SMI_rate, swisscom_r)[1,1]
